8f81ea623ff390ec77034b2c32a5a37763d8bee6
Nils Assel <nils.assel@flyeralarm.com>
2023-08-24 09:19:35 +0200

    Change transaction request to /fetchAll

--- src/data/fetchTransactionObject.ts
+++ src/data/fetchTransactionObject.ts
@@ -3,7 +3,7 @@ import {SetStateAction} from "react";
 import {Item} from "../helper/types";
 
 export const fetchAllDataFromTransactions = async (setStateAction: (value: (((prevState: Item[]) => Item[]) | Item[])) => void) => {
-    let response: AxiosResponse<any> | void = await axios.get<any>("http://localhost:8080/transaction/fetch")
+    let response: AxiosResponse<any> | void = await axios.get<any>("http://localhost:8080/transaction/fetchAll")
         .catch(
             error => console.error(error)
         )
